f8515284e5c8109618da201f995d4096be4dd4f2
Fabien JUIF <fabien.juif@zenika.com>
2017-10-10 16:31:56 +0200

    sparkles: demo / actions / 2

--- demos/index.js
+++ demos/index.js
@@ -5,10 +5,16 @@ const Powerpuff = types
     name: types.string,
     mood: types.enumeration(['happy', 'aggressive']),
   })
+  .actions(self => ({
+    setMood: (mood) => { self.mood = mood },
+  }))
 
 const rebelle = Powerpuff.create({ name: 'Rebelle', mood: 'happy' })
 
 // /!\ doesn't work since we don't use an action to mutate the model
 // rebelle.mood = 'aggressive'
+
+// does work because we use an action
+rebelle.setMood('aggressive')
 
 console.log(JSON.stringify(rebelle.toJSON(), null, 2))
