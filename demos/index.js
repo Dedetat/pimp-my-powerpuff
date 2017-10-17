Dedetat/powerpuff-yourself

sparkles: demo / views / 1

demos/index.js
@@ -1,20 +1,26 @@
+const differenceInCalendarYears = require('date-fns/difference_in_calendar_years')
 const { types } = require('mobx-state-tree')
 
 const Powerpuff = types
   .model({
     name: types.string,
     mood: types.enumeration(['happy', 'aggressive']),
+    birthday: new Date(2014, 4, 18),
   })
   .actions(self => ({
     setMood: (mood) => { self.mood = mood },
   }))
+  .views(self => ({
+    get age() {
+      return differenceInCalendarYears(new Date(), self.birthday)
+    },
+  }))
 
 const powerpuff = Powerpuff.create({ name: 'Rebelle', mood: 'aggressive' })
-
-// /!\ doesn't work since we don't use an action to mutate the model
-// powerpuff.name = 'Belle'
-
-// does work because we use an action
 powerpuff.setMood('happy')
 
+// does print the age (direct access to the view)
+console.log(`${powerpuff.name} is ${powerpuff.age} years old :)`)
+
+// doesnt print the age since this not a snapshot value
 console.log(JSON.stringify(powerpuff.toJSON(), null, 2))

Dedetat/pimp-my-powerpuff

demos/index.js
@@ -1,20 +1,26 @@
+const differenceInCalendarYears = require('date-fns/difference_in_calendar_years')
 const { types } = require('mobx-state-tree')
 
 const Powerpuff = types
   .model({
     name: types.string,
     mood: types.enumeration(['happy', 'aggressive']),
+    birthday: new Date(2014, 4, 18),
   })
   .actions(self => ({
     setMood: (mood) => { self.mood = mood },
   }))
+  .views(self => ({
+    get age() {
+      return differenceInCalendarYears(new Date(), self.birthday)
+    },
+  }))
 
 const rebelle = Powerpuff.create({ name: 'Rebelle', mood: 'happy' })
-
-// /!\ doesn't work since we don't use an action to mutate the model
-// rebelle.mood = 'aggressive'
-
-// does work because we use an action
 rebelle.setMood('aggressive')
 
+// does print the age (direct access to the view)
+console.log(`${rebelle.name} is ${rebelle.age} years old :)`)
+
+// doesnt print the age since this not a snapshot value
 console.log(JSON.stringify(rebelle.toJSON(), null, 2))