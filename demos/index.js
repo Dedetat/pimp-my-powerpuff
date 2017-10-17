Dedetat/powerpuff-yourself

sparkles: demo / model / 2

demos/index.js
@@ -2,13 +2,12 @@ const { types } = require('mobx-state-tree')
 
 const Powerpuff = types
   .model({
-    name: types.string,
+    name: types.maybe(types.string),
   })
 
-// - /!\ doesn't work since `name` is mandatory
-// const powerpuff = Powerpuff.create({ })
+// both do work
+let powerpuff = Powerpuff.create({ })
+console.log(JSON.stringify(powerpuff.toJSON(), null, 2))
 
-// - does work has we fill out the name field
-const powerpuff = Powerpuff.create({ name: 'Rebelle' })
-
+powerpuff = Powerpuff.create({ name: 'Rebelle' })
 console.log(JSON.stringify(powerpuff.toJSON(), null, 2))

Dedetat/pimp-my-powerpuff

demos/index.js
@@ -2,13 +2,12 @@ const { types } = require('mobx-state-tree')
 
 const Powerpuff = types
   .model({
-    name: types.string,
+    name: types.maybe(types.string),
   })
 
-// - /!\ doesn't work since `name` is mandatory
-// const rebelle = Powerpuff.create({ })
+// both do work
+let rebelle = Powerpuff.create({ })
+console.log(JSON.stringify(rebelle.toJSON(), null, 2))
 
-// - does work has we fill out the name field
-const rebelle = Powerpuff.create({ name: 'Rebelle' })
-
+rebelle = Powerpuff.create({ name: 'Rebelle' })
 console.log(JSON.stringify(rebelle.toJSON(), null, 2))
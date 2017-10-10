fb3dc94addd1d3034993a8370e540b361b087114
Fabien JUIF <fabien.juif@zenika.com>
2017-10-10 16:26:28 +0200

    sparkles: demo / model / 2

--- demos/index.js
+++ demos/index.js
@@ -2,13 +2,12 @@ const { types } = require('mobx-state-tree')
 
 const Powerpuff = types
   .model({
-    name: types.string,
+    name: types.maybe(types.string),
   })
 
-// - /!\ doesn't work since `name` is mandatory
-// const rebelle = Powerpuff.create({ })
+// both do work
+let rebelle = Powerpuff.create({ })
+console.log(JSON.stringify(rebelle.toJSON(), null, 2))
 
-// - does work has we fill out the name field
-const rebelle = Powerpuff.create({ name: 'Rebelle' })
-
+rebelle = Powerpuff.create({ name: 'Rebelle' })
 console.log(JSON.stringify(rebelle.toJSON(), null, 2))
